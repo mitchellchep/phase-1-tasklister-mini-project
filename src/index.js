abac7112109586935d1089f776957049c0e4eab7
mitchellkoech <mitchikoechi3.7@gmail.com>
2023-10-17 09:05:37 +0300

    Completed assignment
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Access the input value entered by the user
+    const taskInput = document.getElementById("new-task-description");
+    const taskText = taskInput.value;
+
+    // Create a new task element and add it to the DOM
+    const taskList = document.getElementById("tasks");
+    const newTask = document.createElement("li");
+    newTask.textContent = taskText;
+    taskList.appendChild(newTask);
+
+    // Clear the input field
+    taskInput.value = "";
+  });
 });
+
